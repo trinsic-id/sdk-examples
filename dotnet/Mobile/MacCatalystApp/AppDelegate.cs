ff605410fa0b9a8bc207df51fc78f0b6ea172816
Tomislav Markovski <tomislav@trinsic.id>
2021-12-06 08:55:24 -0500

    MacCatalyst sample

---
+++ dotnet/Mobile/MacCatalystApp/AppDelegate.cs
@@ -0,0 +1,41 @@
+using Trinsic;
+
+namespace MacCatalystApp;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		// create a new window instance based on the screen size
+		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+		// create a UIViewController with a single UILabel
+		var vc = new UIViewController ();
+		vc.View!.AddSubview (new UILabel (Window!.Frame) {
+			BackgroundColor = UIColor.White,
+			TextAlignment = UITextAlignment.Center,
+			Text = "Hello, Catalyst!"
+		});
+		Window.RootViewController = vc;
+
+		// make the window visible
+		Window.MakeKeyAndVisible ();
+
+		SignIn();
+
+		return true;
+	}
+
+	private static async void SignIn()
+	{
+		var account = new AccountService(serverConfig: null);
+		var profile = await account.SignInAsync();
+
+		Console.WriteLine(profile);
+	}
+}
