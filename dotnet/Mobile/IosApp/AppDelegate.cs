b05b6310553e524461fce1ef2d8be68071d834b3
Tomislav Markovski <tomislav@trinsic.id>
2021-12-06 18:12:34 +0000

    [dotnet] Mobile samples for iOS and MacCatalyst with net6 (#12)

---
+++ dotnet/Mobile/IosApp/AppDelegate.cs
@@ -0,0 +1,55 @@
+namespace IosApp;
+
+using Okapi.Keys;
+using Okapi.Keys.V1;
+using Okapi.Security;
+using Okapi.Security.V1;
+using System.Text.Json;
+using Google.Protobuf;
+using Trinsic;
+
+[Register("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate
+{
+    public override UIWindow? Window
+    {
+        get;
+        set;
+    }
+
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+        // create a new window instance based on the screen size
+        Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+        // create a UIViewController with a single UILabel
+        var vc = new UIViewController();
+        vc.View!.AddSubview(new UILabel(Window!.Frame)
+        {
+            BackgroundColor = UIColor.White,
+            TextAlignment = UITextAlignment.Center,
+            Text = "Hello, iOS!"
+        });
+        Window.RootViewController = vc;
+
+        // make the window visible
+        Window.MakeKeyAndVisible();
+
+        SignIn();
+
+        return true;
+    }
+
+    private static async void SignIn()
+    {
+        var accountService = new AccountService(serverConfig: null);
+        var profile = await accountService.SignInAsync();
+
+        Console.WriteLine($"Profile: {profile}");
+
+        var walletService = new WalletService(profile, serverConfig: null);
+        var items = await walletService.Search();
+
+        Console.WriteLine($"Items: {items}");
+    }
+}
---
+++ dotnet/Mobile/MacCatalystApp/AppDelegate.cs
@@ -0,0 +1,46 @@
+using Trinsic;
+
+namespace MacCatalystApp;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		// create a new window instance based on the screen size
+		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+		// create a UIViewController with a single UILabel
+		var vc = new UIViewController ();
+		vc.View!.AddSubview (new UILabel (Window!.Frame) {
+			BackgroundColor = UIColor.White,
+			TextAlignment = UITextAlignment.Center,
+			Text = "Hello, Catalyst!"
+		});
+		Window.RootViewController = vc;
+
+		// make the window visible
+		Window.MakeKeyAndVisible ();
+
+		SignIn();
+
+		return true;
+	}
+
+	private static async void SignIn()
+	{
+		var accountService = new AccountService(serverConfig: null);
+        var profile = await accountService.SignInAsync();
+
+        Console.WriteLine($"Profile: {profile}");
+
+        var walletService = new WalletService(profile, serverConfig: null);
+        var items = await walletService.Search();
+
+        Console.WriteLine($"Items: {items}");
+	}
+}
