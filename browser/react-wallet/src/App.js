2885bce13cc86a58a70921a6ae031b6dbd6a6f36
MichaelEdwardBlack <michaelblack117@gmail.com>
2021-12-13 13:38:03 -0500

    adding router

--- browser/react-wallet/src/App.js
+++ browser/react-wallet/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import TrinsicLogo from './components/Icons/TrinsicLogo';
+import Navbar from './components/Navbar';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen">
+      <BrowserRouter> 
+        <Navbar>
+          <Navbar.Logo href="/#">
+            <TrinsicLogo className="h-12" />
+          </Navbar.Logo>
+          <Navbar.List></Navbar.List>
+        </Navbar>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
