f26d6777a5e1a0691a15e48bfe53187ae284b78f
MichaelEdwardBlack <michaelblack117@gmail.com>
2021-12-14 11:41:12 -0500

    private route

---
+++ browser/react-wallet/src/components/PrivateRoute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect, Route } from "react-router-dom";
+
+function PrivateRoute({ children, loggedIn, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        loggedIn ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+const mapStateToProps = (state) => {
+  const { loggedIn } = state.authentication;
+  return {
+    loggedIn
+  };
+};
+
+export default connect(mapStateToProps, null)(PrivateRoute);
