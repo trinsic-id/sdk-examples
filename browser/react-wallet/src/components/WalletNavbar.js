2834954fc2b4bddff48ec315b6b9c8d6776b19da
MichaelEdwardBlack <michaelblack117@gmail.com>
2021-12-29 13:48:38 -0500

    better sidebar animation

--- browser/react-wallet/src/components/WalletNavbar.js
+++ browser/react-wallet/src/components/WalletNavbar.js
@@ -11,26 +11,26 @@ import {
 export const WalletNavbar = (props) => {
 
   return (
-    <VerticalNavbar id="vertical-navbar" className="bg-white lg:hover:w-64 transition-all duration-500 ease-in-out" {...props}>
-      <VerticalNavbar.List>
+    <VerticalNavbar id="vertical-navbar" className="bg-white w-12 lg:hover:w-64 transition-width duration-700 ease-in-out" {...props}>
+      <VerticalNavbar.List className="flex-nowrap whitespace-nowrap">
         <VerticalNavbar.Item className="justify-end">
           <ChevronDoubleRightIcon className="vertical-navbar-menu-icon"/>
         </VerticalNavbar.Item>
         <VerticalNavbar.Item navigateTo="/issue">
           <CreditCardIcon className="w-8"/>
-          <VerticalNavbar.Link className="pl-2">Issue Credential</VerticalNavbar.Link>
+          <VerticalNavbar.Link className="pl-2 fade-in-text">Issue Credential</VerticalNavbar.Link>
         </VerticalNavbar.Item>
         <VerticalNavbar.Item navigateTo="/verify">
           <BadgeCheck className="w-8"/>
-          <VerticalNavbar.Link className="pl-2">Verify Credential</VerticalNavbar.Link>
+          <VerticalNavbar.Link className="pl-2 fade-in-text">Verify Credential</VerticalNavbar.Link>
         </VerticalNavbar.Item>
         <VerticalNavbar.Item navigateTo="/store">
           <UploadIcon className="w-8"/>
-          <VerticalNavbar.Link className="pl-2">Store Credential</VerticalNavbar.Link>
+          <VerticalNavbar.Link className="pl-2 fade-in-text">Store Credential</VerticalNavbar.Link>
         </VerticalNavbar.Item>
         <VerticalNavbar.Item navigateTo="/proof"> 
           <PencilIcon className="w-8"/>
-          <VerticalNavbar.Link className="pl-2">Generate Proof</VerticalNavbar.Link>
+          <VerticalNavbar.Link className="pl-2 fade-in-text">Generate Proof</VerticalNavbar.Link>
         </VerticalNavbar.Item>
       </VerticalNavbar.List>
     </VerticalNavbar>
