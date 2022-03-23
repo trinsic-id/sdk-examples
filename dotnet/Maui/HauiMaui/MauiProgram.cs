14ea995921b8f7a66df64ea4330e3495b0c92e89
Tomislav Markovski <tomislav@trinsic.id>
2022-03-23 10:07:57 -0400

    [dotnet] add maui sample for mobile

---
+++ dotnet/Maui/HauiMaui/MauiProgram.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+
+namespace HauiMaui;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp() {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        return builder.Build();
+    }
+}
