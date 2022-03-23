536b51af1d784822e1f51ef198929394b94f4269
Tomislav Markovski <tomislav@trinsic.id>
2022-03-23 11:24:18 -0400

    [dotnet] add samples for maui mobile (#42)
    * [dotnet] add maui sample for mobile
    
    * Add readme
    
    * Add note for ios device run

---
+++ dotnet/Maui/HauiMaui/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using Trinsic;
+
+namespace HauiMaui;
+
+public partial class MainPage : ContentPage
+{
+    private AccountService _accountService = new();
+
+    public MainPage() {
+        InitializeComponent();
+    }
+
+    private async void OnSignInClicked(object sender, EventArgs e) {
+        var authToken = await _accountService.SignInAsync(new());
+        AuthTokenLabel.Text = $"Success! [auth_token = {authToken.Substring(0, 16)}...]";
+
+        SemanticScreenReader.Announce(AuthTokenLabel.Text);
+    }
+}
