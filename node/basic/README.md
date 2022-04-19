4c94fd82d3fc432906a2428672780dd0eba7b4e1
Scott Phillips <polygonguru@gmail.com>
2022-04-19 14:12:05 -0400

    make node sample application work

--- node/basic/README.md
+++ node/basic/README.md
@@ -5,4 +5,4 @@ This sample shows how `okapi` can be used in Node.
 To run the sample, build and run using
 
 - `npm i`
-- `node index.js`
+- `node index.ts`
---
+++ node/basic/index.ts
@@ -0,0 +1,38 @@
+import {
+  WalletService,
+  AccountService,
+  SignInRequest,
+  AccountDetails,
+  AccountProfile
+} from "@trinsic/trinsic";
+
+import * as rlsync from "readline-sync";
+
+
+async function main() {
+  console.log(["038205"]);
+  let accountService = new AccountService();
+  let base64Profile = await accountService.signIn(new SignInRequest())
+  accountService.options.setAuthToken(base64Profile);
+
+  let walletService = new WalletService(accountService.options);
+  let searchResponse = await walletService.search();
+  console.log(searchResponse.getItemsList());
+  console.log("Search complete");
+
+  base64Profile = await accountService.signIn(new SignInRequest().setDetails(new AccountDetails().setEmail("scott.phillips@trinsic.id").setName("Scott Phillips")))
+  let code = rlsync.question("Enter the security code sent to your email:") ?? "";
+  let p = AccountProfile.deserializeBinary(Buffer.from(base64Profile, "base64url"));
+  let profile = await accountService.unprotect(p, new TextEncoder().encode(code));
+  const base64UnprotectedProfile = Buffer
+      .from(profile.serializeBinary())
+      .toString('base64url');
+  accountService.options.setAuthToken(base64UnprotectedProfile)
+
+  walletService = new WalletService(accountService.options);
+  searchResponse = await walletService.search();
+  console.log(searchResponse.getItemsList());
+  console.log("Signed in Search complete");
+}
+
+main().then().catch(reason => console.error(reason))
