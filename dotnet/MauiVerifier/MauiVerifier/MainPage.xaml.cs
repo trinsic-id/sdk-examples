a63c5f62f635fa58927e60a13e9b241a2bf5f0e3
Tomislav Markovski <tomislav@trinsic.id>
2022-04-24 12:38:01 -0400

    Verifier Demo for Maui

---
+++ dotnet/MauiVerifier/MauiVerifier/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using AuthenticationServices;
+using Foundation;
+using IdentityModel;
+using IdentityModel.Client;
+using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Browser;
+using IdentityModel.OidcClient.Results;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using UIKit;
+
+namespace MauiVerifier;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage() {
+        InitializeComponent();
+    }
+
+    private async void OnShareCredential(object sender, EventArgs e) {
+        try {
+            OidcClientOptions options = new() {
+                Authority = "https://connect.trinsic.cloud",
+                ClientId = "urn:wallets:123",
+                RedirectUri = "mauiverifier://callback",
+                Browser = new PlatformBrowser(),
+                LoadProfile = false,
+                HttpClientFactory = _ => new HttpClient(new NSUrlSessionHandler()),
+            };
+            OidcClient client = new(options);
+
+            var result = await client.LoginAsync(new LoginRequest {
+                FrontChannelExtraParameters = new Parameters {
+                    { "trinsic:ecosystem", "default" },
+                    { "trinsic:schema", "https://schema.trinsic.cloud/default/attendance-badge" }
+                }
+            });
+
+            if (result.IsError) {
+                ResultLabel.Text = "Action canceled or error occured";
+            }
+            else {
+                ResultLabel.Text = "Success!";
+                ResultEditor.Text = result.IdentityToken;//JsonSerializer.Serialize(token, new JsonSerializerOptions { WriteIndented = true });
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine(ex);
+        }
+    }
+}
---
+++ dotnet/MauiVerifier/MauiVerifier/Platforms/iOS/PlatformBrowser.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using AuthenticationServices;
+using Foundation;
+using IdentityModel.OidcClient.Browser;
+using UIKit;
+
+namespace MauiVerifier;
+
+[SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
+public class PlatformBrowser : IBrowser
+{
+    public Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken = default) {
+        var source = new TaskCompletionSource<BrowserResult>();
+        
+        var session = new ASWebAuthenticationSession(NSUrl.FromString(options.StartUrl), "mauiverifier", (url, error) => {
+            if (error is not null) {
+                source.SetResult(new BrowserResult {
+                    Error = error.LocalizedFailureReason,
+                    ErrorDescription = error.LocalizedDescription,
+                    ResultType = BrowserResultType.UnknownError
+                });
+            } else if (url is not null) {
+                source.SetResult(new BrowserResult {
+                    Response = url.ToString(),
+                    ResultType = BrowserResultType.Success
+                });
+            }
+            else {
+                source.SetResult(new BrowserResult {
+                    ResultType = BrowserResultType.UnknownError,
+                    Error = "Empty response."
+                });
+            }
+        });
+        
+        session.PresentationContextProvider = UIApplication.SharedApplication.Delegate as IASWebAuthenticationPresentationContextProviding;
+
+        session.Start();
+
+        return source.Task;
+    }
+}
