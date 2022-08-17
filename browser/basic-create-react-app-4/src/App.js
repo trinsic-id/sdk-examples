238d98c520b4b9825cbb262918be8c952f3ace1b
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2022-08-17 15:30:11 +0200

    fix: Create React App 4 and restructured browser samples

---
+++ browser/basic-create-react-app-4/src/App.js
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { AccountService } from "@trinsic/trinsic/lib/browser"
+
+const accountService = new AccountService();
+
+function App() {
+  const [authToken, setAuthToken] = useState("");
+  useEffect(() => {
+    const fetchAuthToken = async () => {
+      const authToken = await accountService.signIn();
+      setAuthToken(authToken);
+    };
+    fetchAuthToken().catch((e) => console.error(e));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Your Trinsic Ecosystems Auth Token: {authToken}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
