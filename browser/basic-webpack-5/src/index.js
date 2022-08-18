d98f9a0d74124b4a256335458e9b611c85540f96
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2022-08-18 15:29:22 +0200

    Fine tune webpack 5 sample

--- browser/basic-webpack-5/src/index.js
+++ browser/basic-webpack-5/src/index.js
@@ -1,12 +1,13 @@
 import { AccountService} from "@trinsic/trinsic/browser";
 
 async function signIn() {
-
+    
     const service = new AccountService();
     const configuration = await service.signIn();
-
     console.log(`auth_token = ${configuration}`);
-
+    const info = await service.getInfo();
+    console.log("Account info", info)
+    
     document.getElementById("response").innerText = `auth_token = ${configuration}`
 }
 
--- browser/basic-webpack-5/webpack.config.js
+++ browser/basic-webpack-5/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     })
   ],
   output: {
-    path: path.resolve(__dirname, ""),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   experiments: {
