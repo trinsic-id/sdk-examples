a9b4e4c2e7abb08289c74b8db062423829f49fb2
Tomislav Markovski <tmarkovski@gmail.com>
2022-09-03 19:06:13 +0200

    Add android support

--- dotnet/MauiVerifier/MauiVerifier/Platforms/Android/MainActivity.cs
+++ dotnet/MauiVerifier/MauiVerifier/Platforms/Android/MainActivity.cs
@@ -1,11 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Microsoft.Maui;
+using Android.Content;
+using System;
+using Android.OS;
 
 namespace MauiVerifier
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [IntentFilter(new[] { Intent.ActionView },
+        Categories = new[] { Intent.CategoryBrowsable, Intent.CategoryDefault },
+        DataSchemes = new[] { "mauiverifier" },
+        DataHost = "*"
+    )]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnNewIntent(Intent intent) => PlatformBrowser.ProcessCallback(intent);
     }
 }
--- dotnet/MauiVerifier/MauiVerifier/Platforms/Android/PlatformBrowser.cs
+++ dotnet/MauiVerifier/MauiVerifier/Platforms/Android/PlatformBrowser.cs
@@ -4,38 +4,56 @@ using System.Threading.Tasks;
 using Android.Content;
 using Android.Net;
 using IdentityModel.OidcClient.Browser;
+using Java.Net;
+using Java.Security.Cert;
 using Microsoft.Maui.Controls;
+using static Android.Graphics.ImageDecoder;
 
 namespace MauiVerifier
 {
     public class PlatformBrowser : IBrowser
     {
+        public static TaskCompletionSource<BrowserResult> Source;
         public PlatformBrowser()
         {
         }
 
-        public async Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken = default)
+        public Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken = default)
         {
+            Source = new TaskCompletionSource<BrowserResult>();
+
             try
             {
-                Intent intent = new Intent(Intent.ActionView, Android.Net.Uri.Parse(options.StartUrl));
-                intent.SetFlags(ActivityFlags.NoHistory);
-                intent.SetFlags(ActivityFlags.NewTask);
+                var intent = new Intent(Intent.ActionView, Android.Net.Uri.Parse(options.StartUrl))
+                    .SetFlags(ActivityFlags.NoHistory)
+                    .SetFlags(ActivityFlags.NewTask);
 
                 Android.App.Application.Context.StartActivity(intent);
 
-                await Task.CompletedTask;
-
-                return new BrowserResult
-                {
-                    Response = "OK",
-                    ResultType = BrowserResultType.Success
-                };
+                return Source.Task;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 throw;
+            }
+        }
+
+        public static void ProcessCallback(Intent intent)
+        {
+            if (Source != null)
+            {
+                Source.SetResult(new BrowserResult
+                {
+                    Response = intent.DataString,
+                    ResultType = BrowserResultType.Success
+                });
+
+                Source = null;
+            }
+            else
+            {
+                Console.WriteLine("Warning: PlatformBrowser callback invoked, but Source is null");
             }
         }
     }
