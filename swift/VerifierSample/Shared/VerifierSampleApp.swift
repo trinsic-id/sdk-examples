3a3956d4e71e1f742ccb2e4077b147922992ce5e
Tomislav Markovski <tmarkovski@gmail.com>
2022-09-04 09:44:42 +0200

    Add Swift sample

---
+++ swift/VerifierSample/Shared/VerifierSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerifierSampleApp.swift
+//  Shared
+//
+//  Created by Tomislav Markovski on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct VerifierSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
