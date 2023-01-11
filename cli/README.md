9d1bd39d7444e84ed2f10bb63e7c6ac4632bc98b
Scott Phillips <polygonguru@gmail.com>
2023-01-11 18:48:45 -0500

    Fix up CLI demo with latest powershell test infrastructure demo

--- cli/README.md
+++ cli/README.md
@@ -1,6 +1,6 @@
 # Trinsic CLI Samples
 
-## Dependancies
+## Dependencies
 
 Before you start make sure you have installed the dependencies below:
 
@@ -8,13 +8,9 @@ Before you start make sure you have installed the dependencies below:
 
 - Install the [Trinsic CLI](#)
 
-## Vaccination Sample
+## State ID Sample
 
-This is a simple set of CLI commands that set up three wallets and runs the scenario where
-Alice gets a credential issued by Clinic, and later presents that credential to the Airline.
-
-To run this sample, from  `VaccinationSample` directory run:
+To run this sample, from this directory run:
 ```bash
-chmod +x runthrough.sh
-./runthrough.sh
+pwsh Run-Demo.ps1
 ```
---
+++ cli/Run-Demo.ps1
@@ -0,0 +1,98 @@
+$ErrorActionPreference = "Stop"
+function Stop-OnError {
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error -Message "Script stopped due to CLI error" -ErrorAction Stop
+    }
+}
+
+$ServerEndpoint = "prod.trinsic.cloud"
+
+@{
+    Environment = $Environment
+    "CLI Path" = $CommandPath
+    Version = (Invoke-Expression "$CommandPath --version")
+} | Format-Table
+
+$trinsic = "$CommandPath --json"
+
+Invoke-Expression "$CommandPath config -e $ServerEndpoint" | Stop-OnError | Out-Null
+
+# Print config
+Invoke-Expression "$trinsic config"
+| ConvertFrom-Json
+| Format-List
+Stop-OnError
+
+Write-Output "✅ Creating new ecosystem"
+$CreateEcosystemResponse = Invoke-Expression "$trinsic provider create-ecosystem"
+| ConvertFrom-Json
+Stop-OnError
+$AuthToken = $CreateEcosystemResponse.'auth token'
+Write-Output "`tEcosystem Name = $($CreateEcosystemResponse.ecosystem.name)"
+
+Write-Output "✅ Updating auth token"
+Invoke-Expression "$CommandPath config -a $AuthToken" | Out-Null
+Stop-OnError
+
+Write-Output "✅ Creating new template"
+$Template = Invoke-Expression "$trinsic template create --name 'Iso18013 Drivers License' --fields-file $PSScriptRoot/state-id-template.json --allow-additional"
+| ConvertFrom-Json
+| ForEach-Object { @{ Id = $_.template.data.id; SchemaUri = $_.template.data.schema_uri; ContextUri = $_.template.data.context_uri; } }
+Stop-OnError
+Write-Output "`tTemplate ID = $($Template.Id)"
+Write-Output "`tSchema URI = $($Template.SchemaUri)"
+Write-Output "`tContext URI = $($Template.ContextUri)"
+
+Write-Output "✅ Create trust registry for authorized issuers"
+$FrameworkId = Invoke-Expression "$trinsic trust-registry add-framework --name 'Authorized State ID Issuers' --uri 'https://state.gov/authorized-issuers'"
+| ConvertFrom-Json
+| ForEach-Object { $_.response.id }
+Stop-OnError
+
+Write-Output "✅ Adding trusted issuer to framework"
+$PublicDid = Invoke-Expression "$trinsic account info"
+| ConvertFrom-Json
+| ForEach-Object { $_.'account data'.public_did }
+Stop-OnError
+
+Invoke-Expression "$trinsic trust-registry register-member --schema $($Template.SchemaUri) --framework-id $FrameworkId --did $PublicDid" | Out-Null
+Stop-OnError
+
+Write-Output "✅ Issuing credential for drivers license"
+Invoke-Expression "$trinsic vc issue-from-template --template-id $($Template.Id) --framework-id $FrameworkId --values-file $PSScriptRoot/state-id-values.json"
+| ConvertFrom-Json
+| ForEach-Object { $_.'signed document' }
+| Set-Content -Path $PSScriptRoot/state-id-signed-document.json
+Stop-OnError
+
+Write-Output "✅ Verify credential"
+Invoke-Expression "$trinsic vc verify-proof --proof-document $PSScriptRoot/state-id-signed-document.json"
+| ConvertFrom-Json
+| Select-Object -ExpandProperty 'validation results'
+| Format-List
+Stop-OnError
+
+Write-Output "✅ Create proof of credential"
+Invoke-Expression "$trinsic vc create-proof --document-file $PSScriptRoot/state-id-signed-document.json --out $PSScriptRoot/state-id-proof.json"
+| Out-Null
+Stop-OnError
+
+Write-Output "✅ Verify proof of credential"
+Invoke-Expression "$trinsic vc verify-proof --proof-document $PSScriptRoot/state-id-proof.json"
+| ConvertFrom-Json
+| Select-Object -ExpandProperty 'validation results'
+| Format-List
+Stop-OnError
+
+Write-Output "✅ Remove trusted issuer from framework"
+Invoke-Expression "$trinsic trust-registry unregister-member --schema $($Template.SchemaUri) --framework-id $FrameworkId --did $PublicDid" | Out-Null
+Stop-OnError
+
+Write-Output "✅ Verify proof of credential (should fail 'TrustRegistryMembership' policy)"
+Invoke-Expression "$trinsic vc verify-proof --proof-document $PSScriptRoot/state-id-proof.json"
+| ConvertFrom-Json
+| Select-Object -ExpandProperty 'validation results'
+| Format-List
+Stop-OnError
+
+Write-Output "🎉 Demo complete!"
