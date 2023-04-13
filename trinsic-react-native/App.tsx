30d1357f234bf5a5a0fcdfac7ac1ea8b9b988843
Scott Phillips <polygonguru@gmail.com>
2023-04-13 15:40:44 -0400

    grrr: Android is weird

--- trinsic-react-native/App.tsx
+++ trinsic-react-native/App.tsx
@@ -1,26 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
-import {TrinsicService} from "@trinsic/trinsic";
-
-const trinsicService = new TrinsicService();
+import {CreateWalletRequest, TrinsicService} from "@trinsic/trinsic";
 
 interface DemoData {
   authToken: string;
   accountInfo: string;
 }
 
+function getRuntime(): string {
+    // https://stackoverflow.com/a/39473604
+    if (typeof document !== 'undefined') {
+        // I'm on the web!
+        console.warn("Web!");
+        return "web";
+    }
+    else if (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') {
+        // I'm in react-native
+        console.warn("React native!");
+        return "react-native";
+    }
+    else {
+        console.warn("Node.js!");
+        // I'm in node js
+        return "node";
+    }
+}
+
 async function fetchAuthToken(): Promise<DemoData> {
-    console.log("Creating wallet");
-  const createWalletResponse = await trinsicService.wallet().createWallet({ecosystemId: "default"});
-    console.log("Wallet", createWalletResponse);
-  trinsicService.setAuthToken(createWalletResponse.authToken!);
-  const accountInfo = await trinsicService.wallet().getMyInfo({});
-    console.log("Account information", accountInfo);
-  return {
-    authToken: createWalletResponse.authToken!,
-    accountInfo: JSON.stringify(accountInfo, null, 3)
-  }
+    getRuntime();
+    try {
+        const trinsicService = new TrinsicService();
+        console.log("Service connection", trinsicService);
+        console.log(await trinsicService.provider().getOberonKey());
+        const walletService = trinsicService.wallet();
+        const request = CreateWalletRequest.fromPartial({ecosystemId:"default"});
+        console.log(walletService, CreateWalletRequest.encode(request).finish());
+        const createWalletResponse = await walletService.createWallet(request);
+        console.log("Wallet", createWalletResponse);
+        trinsicService.setAuthToken(createWalletResponse.authToken!);
+        const accountInfo = await trinsicService.wallet().getMyInfo({});
+        console.log("Account information", accountInfo);
+        return {
+            authToken: createWalletResponse.authToken!,
+            accountInfo: JSON.stringify(accountInfo, null, 3)
+        }
+    } catch (e: any) {
+        console.error("Uncaught error", e.stack);
+        return { authToken: "", accountInfo: ""};
+    }
 }
 
   export default function App() {
