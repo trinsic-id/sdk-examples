5af8036bb3ac8ea29a25595461834dac9c5c647c
Scott Phillips <polygonguru@gmail.com>
2023-04-13 12:13:36 -0400

    feat: updated to new consolidated package, works on node and browser! :)

--- node/basic/index.ts
+++ node/basic/index.ts
@@ -1,18 +1,23 @@
-import { TrinsicService } from "@trinsic/trinsic";
+import {IdentityProvider, TrinsicService} from "@trinsic/trinsic";
 
 import * as rlsync from "readline-sync";
 
 async function signedInSearch() {
   let service = new TrinsicService();
-  let loginResponse = await service.account().login({
-    email: "scott.phillips@trinsic.id",
+  let loginResponse = await service.wallet().createWallet({
     ecosystemId: "default"
   });
+  let addEmail = await service.wallet().addExternalIdentityInit({
+    identity: "scott.phillips@trinsic.id",
+    provider: IdentityProvider.EMAIL
+  })
   let code =
     rlsync.question("Enter the security code sent to your email:") ?? "";
-  let loginConfirmResponse = await service
-    .account()
-    .loginConfirm(loginResponse.challenge, code);
+  let emailConfirmed = await service
+    .wallet().addExternalIdentityConfirm({
+        challenge: addEmail.challenge,
+        response: code
+      });
 
   let searchResponse = await service.wallet().searchWallet();
   console.log(searchResponse.items);
@@ -21,8 +26,9 @@ async function signedInSearch() {
 
 async function main() {
   let service = new TrinsicService();
-  const authenticationToken = await service.account().loginAnonymous("default");
-  console.log("Authentication token", authenticationToken)
+  const createWalletResponse = await service.wallet().createWallet({ecosystemId:"default"})
+  console.log("Wallet Created", createWalletResponse)
+  service.setAuthToken(createWalletResponse.authToken!);
 
   let searchResponse = await service.wallet().searchWallet();
   console.log(searchResponse.items);
