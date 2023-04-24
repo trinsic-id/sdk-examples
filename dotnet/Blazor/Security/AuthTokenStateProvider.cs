86f9fb6654bf82be755fb3c9c60f7d30c79250b1
Scott Phillips <polygonguru@gmail.com>
2023-04-24 07:51:37 -0400

    build: Reduce warnings

--- dotnet/Blazor/Security/AuthTokenStateProvider.cs
+++ dotnet/Blazor/Security/AuthTokenStateProvider.cs
@@ -12,18 +12,18 @@ public class AuthTokenStateProvider : AuthenticationStateProvider
 
     private readonly TrinsicService _trinsicService;
 
-    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
     {
         var authToken = _trinsicService.Options.AuthToken;
 
         if (string.IsNullOrWhiteSpace(authToken))
         {
             var anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity() { }));
-            return anonymous;
+            return Task.FromResult(anonymous);
         }
         var userClaimPrincipal = new ClaimsPrincipal(new ClaimsIdentity("OberonAuthentication"));
         var loginUser = new AuthenticationState(userClaimPrincipal);
-        return loginUser;
+        return Task.FromResult(loginUser);
     }
 
     internal void NotifyProfileChanged()
