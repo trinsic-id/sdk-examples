57d8cf6691db25e741e2a3bd46c9887cfdcfa30d
Scott Phillips <polygonguru@gmail.com>
2023-04-24 15:06:55 -0400

    build: Update Android samples to 1.11.0, Update go sample to 1.11.0 (#293)
    * build: Update Android samples to 1.11.0
    
    * fix: Update go sample to 1.11.0

--- go/vaccine_test.go
+++ go/vaccine_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/stretchr/testify/assert"
 	"github.com/trinsic-id/sdk/go/proto/services/provider/v1/provider"
+	"github.com/trinsic-id/sdk/go/proto/services/universalwallet/v1/wallet"
 	"github.com/trinsic-id/sdk/go/services"
 	"testing"
 )
@@ -12,10 +13,7 @@ func makeTestEcosystem(tt *testing.T) (*services.Trinsic, *provider.CreateEcosys
 	trinsic, err := services.NewTrinsic(services.WithTestEnv())
 	assert.NoError(tt, err)
 
-	provider := trinsic.Provider()
-	assert.NoError(tt, err)
-
-	eco, err := provider.CreateEcosystem(context.Background(), nil)
+	eco, err := trinsic.Provider().CreateEcosystem(context.Background(), nil)
 	assert.NoError(tt, err)
 
 	return trinsic, eco
@@ -25,9 +23,9 @@ func createActors(t *testing.T, trinsic *services.Trinsic, ecoId string) map[str
 	actors := []string{"allison", "airline", "clinic"}
 	ret := make(map[string]string)
 	for _, a := range actors {
-		actor, err := trinsic.Account().LoginAnonymous(context.Background(), ecoId)
+		actor, err := trinsic.Wallet().CreateWallet(context.Background(), &wallet.CreateWalletRequest{EcosystemId: ecoId})
 		assert.NoError(t, err)
-		ret[a] = actor
+		ret[a] = actor.AuthToken
 	}
 	return ret
 }
@@ -42,7 +40,7 @@ func TestVaccineDemo(t *testing.T) {
 	assert.Len(t, actors, 3)
 
 	trinsic.SetAuthToken(actors["clinic"])
-	info, err := trinsic.Account().GetInfo(context.Background())
+	info, err := trinsic.Wallet().GetMyInfo(context.Background(), &wallet.GetMyInfoRequest{})
 
 	assert.NotNil(t, info)
 	assert.Nil(t, err)
