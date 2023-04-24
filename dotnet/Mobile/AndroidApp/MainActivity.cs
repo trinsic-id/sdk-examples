8b6a3fc30d93ecc0d71ab95a4132077c66684530
Scott Phillips <polygonguru@gmail.com>
2023-04-24 07:33:06 -0400

    fix: AndroidApp updated to `1.11.0rc1`

--- dotnet/Mobile/AndroidApp/MainActivity.cs
+++ dotnet/Mobile/AndroidApp/MainActivity.cs
@@ -1,5 +1,4 @@
 using Trinsic;
-using Trinsic.Services.UniversalWallet.V1;
 
 namespace AndroidApp
 {
@@ -18,13 +17,12 @@ namespace AndroidApp
 
         private static async void SignIn()
         {
-            var accountService = new AccountService();
-            var authToken = await accountService.LoginAnonymousAsync();
+            var trinsicService = new TrinsicService();
+            var newWallet = await trinsicService.Wallet.CreateWalletAsync(new() { EcosystemId = "default"});
+            trinsicService.Options.AuthToken = newWallet.AuthToken;
+            Console.WriteLine($"AuthToken: {newWallet.AuthToken}");
 
-            Console.WriteLine($"AuthToken: {authToken}");
-
-            var walletService = new WalletService();
-            var items = await walletService.SearchAsync(new ());
+            var items = await trinsicService.Wallet.SearchAsync(new ());
 
             Console.WriteLine($"Items: {items}");
         }
