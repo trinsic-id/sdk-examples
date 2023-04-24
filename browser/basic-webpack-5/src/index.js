05faf951bf172d6f7755253130352e3c7d7d7228
Scott Phillips <polygonguru@gmail.com>
2023-04-24 12:31:52 -0400

    build: Update node/web samples to 1.11rc - Adds `trinsic-react-native` sample (#287)

--- browser/basic-webpack-5/src/index.js
+++ browser/basic-webpack-5/src/index.js
@@ -1,13 +1,13 @@
-import {TrinsicService} from "@trinsic/trinsic/browser";
+import {TrinsicService} from "@trinsic/trinsic";
 
 async function signIn() {
-
     const trinsicService = new TrinsicService();
-    const authenticationToken = await trinsicService.account().loginAnonymous("default");
-    const accountInfo = await trinsicService.account().getInfo();
+    const createWalletResponse = await trinsicService.wallet().createWallet({ecosystemId:"default"});
+    trinsicService.setAuthToken(createWalletResponse.authToken);
+    const accountInfo = await trinsicService.wallet().getMyInfo({});
 
     document.getElementById("accountInfo").innerText = JSON.stringify(accountInfo, null, 4);
-    document.getElementById("authToken").innerText = authenticationToken;
+    document.getElementById("authToken").innerText = createWalletResponse.authToken;
 }
 
 signIn();
---
+++ trinsic-react-native/App.tsx
@@ -0,0 +1,64 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {CreateWalletRequest, TrinsicService} from "@trinsic/trinsic";
+
+interface DemoData {
+  authToken: string;
+  accountInfo: string;
+}
+
+async function fetchAuthToken(): Promise<DemoData> {
+    try {
+        const trinsicService = new TrinsicService();
+        console.log("Service connection", trinsicService);
+        const myKey = (await trinsicService.provider().getOberonKey()).key ?? "";
+        console.log("Oberon key", myKey);
+        const walletService = trinsicService.wallet();
+        const request = CreateWalletRequest.fromPartial({ecosystemId:"default"});
+        console.log(walletService, CreateWalletRequest.encode(request).finish());
+        const createWalletResponse = await walletService.createWallet(request);
+        console.log("Wallet", createWalletResponse);
+        trinsicService.setAuthToken(createWalletResponse.authToken!);
+        const accountInfo = await trinsicService.wallet().getMyInfo({});
+        console.log("Account information", accountInfo);
+        return {
+            authToken: createWalletResponse.authToken!,
+            accountInfo: JSON.stringify(accountInfo, null, 3)
+        }
+    } catch (e: any) {
+        console.error("Uncaught error", e.stack);
+        return { authToken: "", accountInfo: ""};
+    }
+}
+
+  export default function App() {
+    const [info, setInfo] = useState({accountInfo: "", authToken: ""});
+  return (
+      <View style={styles.container}>
+        <Text style={{backgroundColor: "white", width: "50%", color: "red"}}>
+          Authentication token: {info.authToken}
+        </Text>
+        <Text>
+          {info.authToken !== "" && info.accountInfo}
+          {info.authToken === "" && "We're getting your account information"}
+        </Text>
+        <StatusBar style="auto"/>
+        <Button title="Get Auth Token" onPress={async () =>  {
+            console.log("Button press");
+            const newData = await fetchAuthToken();
+            console.log("New data", newData);
+            setInfo(newData);
+        }}/>
+          </View>
+          );
+        }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
