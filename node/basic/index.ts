06049063846468b6b63685537b8f06aa0dd71ba4
Scott Phillips <polygonguru@gmail.com>
2023-06-12 15:29:38 -0400

    no message

--- node/basic/index.ts
+++ node/basic/index.ts
@@ -1,6 +1,22 @@
-import {IdentityProvider, TrinsicService} from "@trinsic/trinsic";
+import {
+  IdentityProvider,
+  TrinsicService
+} from "@trinsic/trinsic";
 
 import * as rlsync from "readline-sync";
+
+const localOptions = {serverUseTls: false,
+  serverEndpoint:"127.0.0.1", serverPort: 5000};
+const devOptions = {serverUseTls: true, serverEndpoint: "dev-internal.trinsic.cloud", serverPort: 443}
+
+async function confirmUnauthenticated() {
+  let service = new TrinsicService(devOptions);
+  const ecosystemId = "default";
+  let addEmail = await service.wallet().addExternalIdentityInit({
+    identity: "jp+test3@trinsic.id",
+    provider: IdentityProvider.EMAIL,
+  });
+}
 
 async function signedInSearch() {
   let service = new TrinsicService({serverUseTls: false,
@@ -37,6 +53,6 @@ async function main() {
   console.log("Search complete");
 }
 
-signedInSearch()
+confirmUnauthenticated()
   .then()
   .catch((reason) => console.error(reason));
