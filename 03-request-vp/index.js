935df7d84862fb13ab66345d50d9c1bdbfe0be61
Tomislav Markovski <tmarkovski@gmail.com>
2023-09-12 16:23:51 -0400

    Cleanup

--- 03-request-vp/index.js
+++ 03-request-vp/index.js
@@ -1,27 +1,26 @@
 import express from 'express';
 import path from 'path';
-import { SignatureType, TrinsicService } from "@trinsic/trinsic";
+import { TrinsicService } from "@trinsic/trinsic";
 
 const app = express();
+app.use(express.json());
 
 const trinsic = new TrinsicService({
     authToken: "<AUTH_TOKEN>",
 });
 
-app.get('/api/verify', async (req, res) => {
+app.post('/api/verify', async (req, res) => {
     const verifyRequest = req.body;
-    
+
     const verifyResponse = await trinsic.credential().verifyProof({
       proofDocumentJson: JSON.stringify(verifyRequest),
     });
 
-    console.log(verifyResponse);
+    res.json(verifyResponse);
 });
 
 // Serve static files from the 'public' folder
 app.use(express.static(process.cwd()));
-// Parse JSON bodies for this app. We're only interested in JSON bodies
-app.use(express.json());
 
 // Catch-all to return index.html for any other request
 app.get('*', (req, res) => {
