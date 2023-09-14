0e70caba9d47ce6578e925c32858547527b0abda
Tomislav Markovski <tmarkovski@gmail.com>
2023-09-14 08:34:23 -0400

    Update

--- 04-identity-verification/index.js
+++ 04-identity-verification/index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { TrinsicService, VerificationType } from "@trinsic/trinsic";
 
 const app = express();
+app.use(express.json());
 
 const trinsic = new TrinsicService({
   authToken: "<AUTH_TOKEN>",
@@ -15,14 +16,19 @@ app.post('/api/create-session', async (req, res) => {
     ]
   });
 
-  console.log(verifyResponse);
   res.json(verifyResponse.session);
+});
+
+app.post('/api/get-session', async (req, res) => {
+  var response = await trinsic.connection().getSession({
+    idvSessionId: req.body.idvSessionId
+  });
+
+  req.json(response);
 });
 
 // Serve static files from the 'public' folder
 app.use(express.static(process.cwd()));
-// Parse JSON bodies for this app. We're only interested in JSON bodies
-app.use(express.json());
 
 // Catch-all to return index.html for any other request
 app.get('*', (req, res) => {
