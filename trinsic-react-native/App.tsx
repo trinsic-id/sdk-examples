72240819f5cc1c459b270eb01402dd7a34917ecc
chiarapaskulin <chiarapaskulin@gmail.com>
2023-09-25 12:28:29 -0300

    Docs improvements (#295)
    * Update index.js
    
    * standardizing trinsic service's name
    
    * remove unknown files

--- trinsic-react-native/App.tsx
+++ trinsic-react-native/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
-import {CreateWalletRequest, TrinsicService} from "@trinsic/trinsic";
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { CreateWalletRequest, TrinsicService } from "@trinsic/trinsic";
 
 interface DemoData {
   authToken: string;
@@ -9,50 +9,50 @@ interface DemoData {
 }
 
 async function fetchAuthToken(): Promise<DemoData> {
-    try {
-        const trinsicService = new TrinsicService();
-        console.log("Service connection", trinsicService);
-        const myKey = (await trinsicService.provider().getOberonKey()).key ?? "";
-        console.log("Oberon key", myKey);
-        const walletService = trinsicService.wallet();
-        const request = CreateWalletRequest.fromPartial({ecosystemId:"default"});
-        console.log(walletService, CreateWalletRequest.encode(request).finish());
-        const createWalletResponse = await walletService.createWallet(request);
-        console.log("Wallet", createWalletResponse);
-        trinsicService.setAuthToken(createWalletResponse.authToken!);
-        const accountInfo = await trinsicService.wallet().getMyInfo({});
-        console.log("Account information", accountInfo);
-        return {
-            authToken: createWalletResponse.authToken!,
-            accountInfo: JSON.stringify(accountInfo, null, 3)
-        }
-    } catch (e: any) {
-        console.error("Uncaught error", e.stack);
-        return { authToken: "", accountInfo: ""};
+  try {
+    const trinsic = new TrinsicService();
+    console.log("Service connection", trinsic);
+    const myKey = (await trinsic.provider().getOberonKey()).key ?? "";
+    console.log("Oberon key", myKey);
+    const walletService = trinsic.wallet();
+    const request = CreateWalletRequest.fromPartial({ ecosystemId: "default" });
+    console.log(walletService, CreateWalletRequest.encode(request).finish());
+    const createWalletResponse = await walletService.createWallet(request);
+    console.log("Wallet", createWalletResponse);
+    trinsic.setAuthToken(createWalletResponse.authToken!);
+    const accountInfo = await trinsic.wallet().getMyInfo({});
+    console.log("Account information", accountInfo);
+    return {
+      authToken: createWalletResponse.authToken!,
+      accountInfo: JSON.stringify(accountInfo, null, 3)
     }
+  } catch (e: any) {
+    console.error("Uncaught error", e.stack);
+    return { authToken: "", accountInfo: "" };
+  }
 }
 
-  export default function App() {
-    const [info, setInfo] = useState({accountInfo: "", authToken: ""});
+export default function App() {
+  const [info, setInfo] = useState({ accountInfo: "", authToken: "" });
   return (
-      <View style={styles.container}>
-        <Text style={{backgroundColor: "white", width: "50%", color: "red"}}>
-          Authentication token: {info.authToken}
-        </Text>
-        <Text>
-          {info.authToken !== "" && info.accountInfo}
-          {info.authToken === "" && "We're getting your account information"}
-        </Text>
-        <StatusBar style="auto"/>
-        <Button title="Get Auth Token" onPress={async () =>  {
-            console.log("Button press");
-            const newData = await fetchAuthToken();
-            console.log("New data", newData);
-            setInfo(newData);
-        }}/>
-          </View>
-          );
-        }
+    <View style={styles.container}>
+      <Text style={{ backgroundColor: "white", width: "50%", color: "red" }}>
+        Authentication token: {info.authToken}
+      </Text>
+      <Text>
+        {info.authToken !== "" && info.accountInfo}
+        {info.authToken === "" && "We're getting your account information"}
+      </Text>
+      <StatusBar style="auto" />
+      <Button title="Get Auth Token" onPress={async () => {
+        console.log("Button press");
+        const newData = await fetchAuthToken();
+        console.log("New data", newData);
+        setInfo(newData);
+      }} />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
